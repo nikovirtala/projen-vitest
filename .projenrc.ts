36dfaab2aa468318b7e5ee6788ff3f9ca11a43d1
Niko Virtala <niko.virtala@hey.com>
2024-12-28 17:01:31 +0200

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,64 @@
+import { cdk, javascript, TextFile } from "projen";
+
+const nodeVersion = "22.12.0";
+
+const project = new cdk.JsiiProject({
+    author: "Niko Virtala",
+    authorAddress: "niko.virtala@hey.com",
+    defaultReleaseBranch: "main",
+    deps: ["projen"],
+    description: "",
+    dependabot: false,
+    depsUpgradeOptions: {
+        workflowOptions: {
+            labels: ["auto-approve", "auto-merge"],
+        },
+    },
+    autoApproveOptions: {
+        secret: "GITHUB_TOKEN",
+        allowedUsernames: ["nikovirtala"],
+    },
+    mergify: true,
+    autoMerge: true,
+    jsiiVersion: "~5.7.0",
+    license: "MIT",
+    licensed: true,
+    minNodeVersion: nodeVersion,
+    name: "projen-vitest",
+    npmAccess: javascript.NpmAccess.PUBLIC,
+    packageManager: javascript.NodePackageManager.PNPM,
+    packageName: "@nikovirtala/projen-vitest",
+    peerDeps: ["projen", "constructs"],
+    pnpmVersion: "9",
+    prettier: true,
+    prettierOptions: {
+        settings: {
+            printWidth: 120,
+            tabWidth: 4,
+            trailingComma: javascript.TrailingComma.ALL,
+        },
+    },
+    projenrcTs: true,
+    releaseToNpm: true,
+    repositoryUrl: "https://github.com/nikovirtala/projen-vitest.git",
+    typescriptVersion: "5.7.2",
+});
+
+project.vscode?.extensions.addRecommendations("dbaeumer.vscode-eslint", "esbenp.prettier-vscode");
+
+project.vscode?.settings.addSettings({
+    "editor.codeActionsOnSave": {
+        "source.fixAll": "explicit",
+    },
+    "editor.defaultFormatter": "esbenp.prettier-vscode",
+    "editor.formatOnSave": true,
+    "editor.tabSize": 4,
+});
+
+new TextFile(project, ".nvmrc", {
+    committed: true,
+    readonly: true,
+    lines: ["v" + nodeVersion],
+});
+
+project.synth();
